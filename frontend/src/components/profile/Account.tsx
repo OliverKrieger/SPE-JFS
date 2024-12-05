014ace23a06d70f841e15d8e37c142592bf63056
Oliver Krieger <oliver.krieger76@gmail.com>
2024-12-05 13:12:43 +0000

    add account details and delete hook to account

--- frontend/src/components/profile/Account.tsx
+++ frontend/src/components/profile/Account.tsx
@@ -1,21 +1,51 @@
+import axios from "axios";
+
+import { useAlert } from "../../context/AlertContext";
 import { useOverlay } from "../../context/OverlayContext";
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../../store/store';
+import { logout } from "../../store/authSlice";
 
 import ConfirmationDialog from "../utils/ConfirmationDialog";
 
 const Account: React.FC = () => {
     const { openOverlay, closeOverlay } = useOverlay();
+    const user = useSelector((state: RootState) => state.auth.user);
 
-    const handleConfirm = () => {
-        console.log('User clicked Yes');
+    const { addAlert } = useAlert();
+    const dispatch = useDispatch();
+
+    const apiURL = import.meta.env.VITE_API_URL || "http://localhost:5000"
+
+    const handleConfirm = async () => {
+        try {
+            const response = await axios.post( apiURL+'/auth/delete', {}, { withCredentials: true });
+
+            if (response.data.message) {
+                addAlert(`Account deleted successfully.`, 'success');
+                dispatch(logout());
+            }
+        } catch (error: any) {
+            addAlert(error.response?.data?.error || 'An error occurred during deleting the account.', 'error');
+        }
     };
 
     const handleCancel = () => {
-        console.log('User clicked No');
         closeOverlay();
     };
 
     return (
         <div className='account-container'>
+            {user ? (
+                <div>
+                    <span>{user.username}</span>
+                    <span>{user.faction}</span>
+                </div>
+            ) : (
+                <div>
+                    <span>Error</span>
+                </div>
+            )}
             <button onClick={() => openOverlay(
                 <ConfirmationDialog
                     message="Are you sure you want to delete your account?"
